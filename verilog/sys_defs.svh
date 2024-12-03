f2fe7adc9ed92cca4bbb317aa78760a1c5c3c994
ffdm <fdm@umich.edu>
2024-12-02 23:26:57 -0500

    Add input buffer defs

--- verilog/sys_defs.svh
+++ verilog/sys_defs.svh
@@ -3,10 +3,12 @@
 
 `timescale 1ns/100ps
 
-`define Q  8        // Precision (bits)
-`define H  3        // Size of Huffman table (number of entries)
-`define IN 32       // Int to decoder (bits per cycle)
-`define PERIOD 20   // Clock period (ns)
+`define Q  8                // Precision (bits)
+`define H  3                // Size of Huffman table (number of entries)
+`define IN 32               // Int to decoder (bits per cycle)
+`define PERIOD 20           // Clock period (ns)
+`define IN_BUS_WIDTH 32     // Input bus bit width 
+`define IN_BUFF_SIZE 48     // Size of input buffer
 
 typedef struct packed {
     logic [3:0]  size;
